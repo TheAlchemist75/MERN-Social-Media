8181412ffdd61288e5201f02de5962aab6203d27
Jay Aher <jaysureshaher@gmail.com>
2023-04-19 01:05:17 +0530

    Username function added

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,52 +1,61 @@
-import React, { useState } from "react";
-import "./Post.css";
-import Comment from "../../img/comment.png";
-import Share from "../../img/share.png";
-import Heart from "../../img/like.png";
-import NotLike from "../../img/notlike.png";
-import { likePost } from "../../api/PostsRequests";
-import { useSelector } from "react-redux";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { likePost } from '../../api/PostsRequests';
+import Comment from '../../img/comment.png';
+import Heart from '../../img/like.png';
+import NotLike from '../../img/notlike.png';
+import Share from '../../img/share.png';
+import './Post.css';
 
 const Post = ({ data }) => {
-  const { user } = useSelector((state) => state.authReducer.authData);
-  const [liked, setLiked] = useState(data.likes.includes(user._id));
-  const [likes, setLikes] = useState(data.likes.length)
+	const { user } = useSelector((state) => state.authReducer.authData);
+	const [liked, setLiked] = useState(data.likes.includes(user._id));
+	const [likes, setLikes] = useState(data.likes.length);
 
-  
-  const handleLike = () => {
-    likePost(data._id, user._id);
-    setLiked((prev) => !prev);
-    liked? setLikes((prev)=>prev-1): setLikes((prev)=>prev+1)
-  };
-  return (
-    <div className="Post">
-      <img
-        src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ""}
-        alt=""
-      />
+	axios
+		.get(`http://localhost:5000/user/${data.userId}`)
+		.then(function (response) {
+			// handle success
+			console.log(response);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		})
+		.then(function () {
+			// always executed
+			// const userName = response.data.username;
+		});
 
-      <div className="postReact">
-        <img
-          src={liked ? Heart : NotLike}
-          alt=""
-          style={{ cursor: "pointer" }}
-          onClick={handleLike}
-        />
-        <img src={Comment} alt="" />
-        <img src={Share} alt="" />
-      </div>
+	const handleLike = () => {
+		likePost(data._id, user._id);
+		setLiked((prev) => !prev);
+		liked ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
+	};
+	return (
+		<div className='Post'>
+			<div className='username'>
+				<b>{userName}</b>
+			</div>
+			<div className='detail'>
+				<span>
+					<b>{data.name} </b>
+				</span>
+				<span>{data.desc}</span>
+			</div>
 
-      <span style={{ color: "var(--gray)", fontSize: "12px" }}>
-        {likes} likes
-      </span>
-      <div className="detail">
-        <span>
-          <b>{data.name} </b>
-        </span>
-        <span>{data.desc}</span>
-      </div>
-    </div>
-  );
+			<img src={data.image ? process.env.REACT_APP_PUBLIC_FOLDER + data.image : ''} alt='' />
+
+			<div className='postReact'>
+				<img src={liked ? Heart : NotLike} alt='' style={{ cursor: 'pointer' }} onClick={handleLike} />
+				<img src={Comment} alt='' />
+				<img src={Share} alt='' />
+			</div>
+
+			<span style={{ color: 'var(--gray)', fontSize: '12px' }}>{likes} likes</span>
+		</div>
+	);
 };
 
 export default Post;
